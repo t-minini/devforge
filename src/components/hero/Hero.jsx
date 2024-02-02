30340b9dda4558548696cb1d5a9e44757842d1f2
Tulio <tulio.mminini@gmail.com>
2024-02-02 20:05:34 +0000

    changing offset value for Link

--- src/components/hero/Hero.jsx
+++ src/components/hero/Hero.jsx
@@ -8,7 +8,7 @@ export function Hero() {
       <div className={style.hero__content}>
         <h1>Welcome to</h1>
         <img src={heroLogo} alt="" />
-        <Link to="why" offset={-90} smooth={true} className={style.hero__link}>
+        <Link to="why" offset={-80} smooth={true} className={style.hero__link}>
           Learn Now
         </Link>
       </div>
