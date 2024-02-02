5d4512e9b444ab858e38c37afe5f5e13bff45d92
Tulio <tulio.mminini@gmail.com>
2024-02-02 19:48:52 +0000

    Why component finished

--- src/components/what/What.jsx
+++ src/components/what/What.jsx
@@ -2,7 +2,10 @@ import style from './What.module.css';
 import logo from './../../assets/images/navbar-logo.svg';
 import one from './../../assets/images/what-images/what-01.jpg';
 import two from './../../assets/images/what-images/what-02.jpg';
-
+import three from './../../assets/images/what-images/what-03.jpg';
+import four from './../../assets/images/what-images/what-04.jpg';
+import five from './../../assets/images/what-images/what-05.jpg';
+import six from './../../assets/images/what-images/what-06.jpg';
 
 export function What() {
   return (
@@ -32,7 +35,66 @@ export function What() {
             <p>02</p>
             <h3>Web Development</h3>
             <p>
-            Dive into front-end web development with HTML, CSS, and JavaScript. Master back-end development using Python, Ruby, or NodeJS and explore frameworks like React, Angular, or Django to build dynamic web applications.
+              Dive into front-end web development with HTML, CSS, and
+              JavaScript. Master back-end development using Python, Ruby, or
+              NodeJS and explore frameworks like React, Angular, or Django to
+              build dynamic web applications.
+            </p>
+          </div>
+        </div>
+        <div className={style.content__left}>
+          <div className={style.left__text}>
+            <img src={logo} alt="" />
+            <p>03</p>
+            <h3>Mobile App Development</h3>
+            <p>
+              Create mobile apps for Android and iOS platforms using
+              industry-leading tools and frameworks.
+            </p>
+          </div>
+          <div className={style.left__img}>
+            <img src={three} alt="" />
+          </div>
+        </div>
+        <div className={style.content__right}>
+          <div className={style.right__img}>
+            <img src={four} alt="" />
+          </div>
+          <div className={style.right__text}>
+            <img src={logo} alt="" />
+            <p>04</p>
+            <h3>Full-Stack Development</h3>
+            <p>
+              Become a versatile developer by mastering both front-end and
+              back-end technologies.
+            </p>
+          </div>
+        </div>
+        <div className={style.content__left}>
+          <div className={style.left__text}>
+            <img src={logo} alt="" />
+            <p>05</p>
+            <h3>Database Management</h3>
+            <p>
+              Learn to design and manage databases using SQL and NoSQL
+              technologies.
+            </p>
+          </div>
+          <div className={style.left__img}>
+            <img src={five} alt="" />
+          </div>
+        </div>
+        <div className={style.content__right}>
+          <div className={style.right__img}>
+            <img src={six} alt="" />
+          </div>
+          <div className={style.right__text}>
+            <img src={logo} alt="" />
+            <p>06</p>
+            <h3>DevOps and Agile Practices</h3>
+            <p>
+              Discover the principles of DevOps and Agile methodologies to
+              streamline development processes.
             </p>
           </div>
         </div>
