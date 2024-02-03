2bcb7c1768d755f9af2f8d297073d7a5b333836f
Tulio <tulio.mminini@gmail.com>
2024-02-03 11:04:02 +0000

    Misson component complete

--- src/components/mission/Mission.jsx
+++ src/components/mission/Mission.jsx
@@ -3,7 +3,17 @@ import style from './Mission.module.css';
 export function Mission() {
   return (
     <section id="mission" className={style.mission}>
-      <h1>MISSION SECTION</h1>
+      <div className={style.mission__content}>
+        <h2>Our Mission</h2>
+        <p>
+          At devForge, we are passionate about empowering individuals with the
+          knowledge and skills needed to thrive in the world of software
+          development. Our comprehensive software development training programs
+          are designed to equip aspiring developers and seasoned professionals
+          alike with the expertise they need to succeed in the fast-paced tech
+          industry.
+        </p>
+      </div>
     </section>
   );
 }
