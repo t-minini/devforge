4b74a7e98726df3b7413f21ceedd73b25acbfa63
Tulio <tulio.mminini@gmail.com>
2024-02-03 11:58:01 +0000

    Footer component started

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,9 +1,44 @@
 import style from './Footer.module.css';
+import linkedin from './../../assets/images/linkedin-icon.svg';
+import logo from './../../assets/images/footer-logo.svg';
+import { Link } from 'react-scroll';
 
 export function Footer() {
   return (
-    <section id="footer" className={style.footer}>
-      <h1>FOOTER SECTION</h1>
-    </section>
+    <footer id="footer" className={style.contact}>
+      <div className={style.contact__content}>
+        <div className={style.content__top}>
+          <h2>
+            Get started on your <br />
+            software development journey.
+          </h2>
+          <p>
+            Whether you're aiming to build a career as a software developer or
+            seeking to enhance your existing skills, devForge's software
+            development training programs are the perfect launchpad. Join us
+            today and embark on a transformative learning experience that will
+            set you on the path to success.
+          </p>
+        </div>
+        <div>
+          <p>Contact us for more information</p>
+          <p>+353 86 342 0309</p>
+          <p>alan@devforge.ie</p>
+          <img src={linkedin} alt="" />
+        </div>
+        <div>
+          <img src={logo} alt="" />
+          <p>devForge Limited</p>
+          <p>Athlone, Co. Westmeath</p>
+        </div>
+        <hr />
+        <div>
+          <p>Copyright © devForge Limited 2024</p>
+          <Link to="hero" spy={true} offset={-80} smooth={true}>
+            back to top
+          </Link>
+        </div>
+      </div>
+    </footer>
   );
 }
